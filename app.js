865103b0b1b495a1631d8245d325a2bf7a250aed
GAM3GENIE <davidlbernardino@gmail.com>
2022-05-15 10:06:25 -0700

    coding secret message

---
+++ app.js
@@ -0,0 +1,27 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+console.log(secretMessage.length);
+
+secretMessage.pop();
+
+console.log(secretMessage.length);
+
+secretMessage.push('to', 'Program');
+
+console.log(secretMessage.length);
+
+secretMessage.splice(7, 1, 'right');
+
+console.log(secretMessage);
+
+secretMessage.shift();
+
+console.log(secretMessage);
+
+secretMessage.unshift('Programming');
+
+console.log(secretMessage);
+
+secretMessage.splice(5, 6, 'know');
+
+console.log(secretMessage.join(' '));
